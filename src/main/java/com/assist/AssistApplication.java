3021a2885223c34c03c3fd9231a79beca6f52daa
DESKTOP-848094R\super <superjun02@naver.com>
2021-11-12 19:25:49 +0900

    AWS용 WAR 세팅

--- src/main/java/com/assist/AssistApplication.java
+++ src/main/java/com/assist/AssistApplication.java
@@ -2,9 +2,10 @@ package com.assist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AssistApplication {
+public class AssistApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AssistApplication.class, args);
